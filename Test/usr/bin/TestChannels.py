8f5991fef9da0f79b040a43a5d5d647afe732fb9
blues-alex <ya.blues@yandex.ru>
2023-05-18 14:09:02 +0400

    TestChannels

--- Test/usr/bin/TestChannels.py
+++ Test/usr/bin/TestChannels.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 from LMInterface import *
 # from loguru import *
@@ -8,11 +8,11 @@ CHANNELS = 12
 h = LMHandler()
 h.SM(2)
 for ch in range(CHANNELS):
-    
-    h.SC([0]*ch + [0.1] +[0]*(CHANNELS - ch - 1))
+
+    h.SC([0] * ch + [0.1] + [0] * (CHANNELS - ch - 1))
     print(f"Канал {ch+1} PWM: Нажмите ENTER что бы установить 23% в канале.")
     input()
-    h.SC([0]*ch + [23] +[0]*(CHANNELS - ch - 1))
+    h.SC([0] * ch + [23] + [0] * (CHANNELS - ch - 1))
     print(f"Канал {ch+1} Аналог(23%) : Нажмите ENTER что бы перейти к следующему каналу.")
     input()
 
