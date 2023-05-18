6fba5c48c2fa6bd86a8afa705a3951f65348fdae
blues-alex <ya.blues@yandex.ru>
2023-05-18 13:06:57 +0400

    fix Cycle checker script

--- Test/usr/bin/CheckCycle_300.py
+++ Test/usr/bin/CheckCycle_300.py
@@ -3,7 +3,7 @@
 import time
 import sys
 import json
-# import random
+import random
 from LMInterface import *
 from loguru import *
 # import plotly.graph_objects as go
@@ -18,52 +18,83 @@ CYCLE_LOG = '/tmp/cycle.json'
 handler = LMHandler()
 
 # Make cycle "saw" 78 points, in range(0-100 %%) of all channels
-LENGTH = 48
+LENGTH = 78
 TIME_STEP = (60 * 24) // (LENGTH + 1)
+OFFSET = (60 * 60 * 24) - 120  # offset time of start test in seconds
 HIGHT_EMISSION = 40
 NULL_EMISSION = 0
-TIMEOUT = 5
+TIMEOUT = 5  # random timeout requests in range
 # DAYTIME = 60*24
+
+
+def get_timeline(length) -> list:
+    ts = 0
+    for n in range(length):
+        _range = (60 * 24 - ts) // (length - n)
+        print(f"{_range = }")
+        ts += random.randint(1, _range)
+        if ts < (60 * 24):
+            yield ts
 
 
 def gen_cycle():
     state = HIGHT_EMISSION
     cycle = []
-    tss = []
-    for i in range(1, LENGTH + 1, 1):
-        # ts = f"{i*TIME_STEP//60:02d}{i*TIME_STEP%60:02d}"
-        # fig.update()
+    for i in get_timeline(LENGTH):
         cycle.append(
-            [i * TIME_STEP] + [state] * 12
+            [i] + [state * random.random()] * 12
         )
         if state == HIGHT_EMISSION:
             state = NULL_EMISSION
         else:
             state = HIGHT_EMISSION
-        tss.append(i * TIME_STEP)
-        # fig.data[0].y = [i[1] for i in cycle]
-        # fig.data[0].x = [i[0] for i in cycle]
     return cycle
+
+# def gen_cycle():
+#     cycle = []
+#     for i in range(0, LENGTH, 3):
+#         cycle.append(
+#             [i * TIME_STEP] + [HIGHT_EMISSION] * 12
+#         )
+#         cycle.append(
+#             [round((i + 1.5) * TIME_STEP)] + [HIGHT_EMISSION] * 12
+#         )
+#         cycle.append(
+#             [round((i + 1.5) * TIME_STEP) + 1] + [NULL_EMISSION] * 12
+#         )
+#         cycle.append(
+#             [(i + 3) * TIME_STEP - 1] + [NULL_EMISSION] * 12
+#         )
+#     return cycle
 
 
 def get_interpolate_channels(ts, cycle) -> list:
-    for n, p in enumerate(cycle):
-        next_point = cycle[n + 1]
-        if next_point[0] * 60 > ts:
-            # print(p, next_point)
-            delta = ts - p[0] * 60
-            angle = (next_point[1] - p[1]) / ((next_point[0] * 60) - (p[0] * 60))
-            # print(f"{delta =}, {angle =}")
-            return [round(p[1] + (delta * angle), 2)] * 12
+    if (ts / 60) > cycle[-1][0] or (ts / 60) < cycle[0][0]:
+        if (ts / 60) < cycle[0][0]:
+            delta_time = (((60 * 60 * 24) - cycle[-1][0] * 60) + ts)
+        else:
+            delta_time = ts - cycle[-1][0] * 60
+        angle = (cycle[-1][1] - cycle[0][1]) / (((60 * 60 * 24) - (cycle[-1][0] * 60)) + (cycle[0][0] * 60))
+        return [round(abs(cycle[-1][1] + (delta_time * angle)), 2)] * 12
+    else:
+        for n, p in enumerate(cycle):
+            next_point = cycle[n + 1]
+            if next_point[0] * 60 > ts:
+                # print(p, next_point)
+                delta = ts - p[0] * 60
+                angle = (next_point[1] - p[1]) / ((next_point[0] * 60) - (p[0] * 60))
+                # print(f"{delta =}, {angle =}")
+                return [round(p[1] + (delta * angle), 2)] * 12
 
 
 def get_controller_ts() -> int:
     gt = handler.GT()['mess'][:4]
-    print(gt)
+    # print(gt)
     hh = int(gt[:2])
     mm = int(gt[2:])
+    # ss = int(gt[4:])
     minutes = (hh * 60) + mm
-    seconds = minutes * 60
+    seconds = minutes * 60  # + ss
     # logger.info(f"{hh:02d}:{mm:02d}, {minutes = }, {seconds = }")
     return seconds
 
@@ -71,32 +102,40 @@ def get_controller_ts() -> int:
 t = time.monotonic()
 
 cycle = gen_cycle()
-# fig.show()
+
 with open(CYCLE_LOG, 'w') as j:
     json.dump(
         [[i[0] * 60] + i[1:] for i in cycle],
         j
     )
 
-print(f"Start writting cycle: {time.monotonic() - t}")
-if not handler.writeCycle(cycle=[[f"{i[0]//60:02d}{i[0]%60:02d}"] + i[1:] for i in cycle]):
-    sys.exit(1)
-logger.success(f"Done writting: {time.monotonic() - t}")
-# logger.info(str(wc))
-# logger.info(f'ST: {TIME_STEP*3//60:02d}{TIME_STEP*3%60:02d}00000000')
-handler.ST(f'{TIME_STEP*3//60:02d}{TIME_STEP*3%60-1:02d}00000000')
 tm = time.monotonic()
-current_time = TIME_STEP * 3 * 60 - 60
+# current_time = TIME_STEP * (LENGTH) * 60 - 60
+current_time = OFFSET
+logger.info(f"SetTime: {current_time//60//60:02d}{current_time//60%60:02d}00000000")
+
+handler.ST(f'{current_time//60//60:02d}{current_time//60%60:02d}00000000')
 tm_start = tm - current_time
-ct = handler.GT()['mess'][:3]
+
+ct = handler.GT()['mess'][:4]
 print(
     f"Controll time: "
     f"{int(ct[:1]):02d}:{int(ct[2:]):02d} ({int(ct[:1]) * 60 + int(ct[2:])}), "
     f"{tm_start}, {tm}, {tm-tm_start}"
 )
-time.sleep(1)
-current_time += 1
-# meassures = {}
+
+print(f"Start writting cycle: {time.monotonic() - t}")
+if not handler.writeCycle(cycle=[[f"{i[0]//60:02d}{i[0]%60:02d}"] + i[1:] for i in cycle]):
+    sys.exit(1)
+logger.success(f"Done writting: {time.monotonic() - t}")
+gc = [
+    (10000 - int(i)) / 100
+    for i in handler.GC()['mess'].split(b'\x1f')
+]
+logger.info(f"Controll GC: {gc}")
+
+current_time = get_controller_ts()
+
 with open(DELTA_LOG, 'w') as j:
     interpolate_chns = get_interpolate_channels(current_time, cycle)
     chns = [(10000 - int(i)) / 100 for i in handler.GC()['mess'].split(b'\x1f')]
@@ -104,18 +143,25 @@ with open(DELTA_LOG, 'w') as j:
             f"{interpolate_chns},"
             f"{chns}]" + "}"
             )
-
+timers = []
+time.sleep(5)
 while True:
+    # gt = get_controller_ts()
+    if time.monotonic() - tm_start > (60 * 60 * 24):
+        tm_start += (60 * 60 * 24)
+    current_time = time.monotonic() - tm_start
     interpolate_chns = get_interpolate_channels(current_time, cycle)
-    gt = get_controller_ts()
+    # ct = time.monotonic()
     chns = [(10000 - int(i)) / 100 for i in handler.GC()['mess'].split(b'\x1f')]
-    print(f"Interpolate:\t[{current_time}]\t{interpolate_chns}")
-    print(f"Controller:\t[{gt}]\t{chns}")
+    # print(ct - time.monotonic())
+    print(f"Int-e:\t[{round(current_time)}]\t{interpolate_chns}")
+    print(f"Cntrl:\t[{get_controller_ts()}]\t{chns}\n")
     with open(DELTA_LOG, 'a') as j:
         j.seek(j.truncate(j.tell() - 1))
-        # meassures[current_time] = [interpolate_chns, chns]
-        j.write(f",\"{current_time}\":[{interpolate_chns},{chns}]")
+        j.write(f",\"{round(current_time)}\":[{interpolate_chns},{chns}]")
         j.write("}")
-    # print(meassures)
-    time.sleep(TIMEOUT)
-    current_time += TIMEOUT
+    timeout = random.randint(0, TIMEOUT)
+    timers.append(timeout)
+    time.sleep(timeout)
+    print(f"Intermediate timeout: {sum(timers)/len(timers)}")
+    # current_time += timeout
